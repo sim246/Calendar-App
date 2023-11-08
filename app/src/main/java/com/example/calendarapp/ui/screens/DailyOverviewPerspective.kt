85435f0d46b1c1af423e0db7d827894caf8d411e
Simona Georgiana Dragomir <simona.dragomir@dawsoncollege.qc.ca>
2023-11-08 11:20:23 -0500

    displaying the hours

--- app/src/main/java/com/example/calendarapp/ui/screens/DailyOverviewPerspective.kt
+++ app/src/main/java/com/example/calendarapp/ui/screens/DailyOverviewPerspective.kt
@@ -1,22 +1,27 @@
 package com.example.calendarapp.ui.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun DailyOverview() {
 
     var hour = 6;
+    var meridiem = "AM"
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
+        horizontalAlignment = Alignment.Start,
         modifier = Modifier
             .background(MaterialTheme.colorScheme.background)
             .fillMaxSize()
@@ -25,12 +30,19 @@ fun DailyOverview() {
         var i = 0;
         while (i <= 18)
         {
-            Row()
+            Row(horizontalArrangement = Arrangement.Start)
             {
-                Text("$hour:00")
+                Text("$hour:00 $meridiem")
             }
             i++
-            hour++
+            if (hour == 12)
+            {
+                hour = 1
+                meridiem = "PM"
+            } else
+            {
+                hour++
+            }
         }
     }
 }
