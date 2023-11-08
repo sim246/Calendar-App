ccb835270d9f38f3d41746b7b65c822bfc1896d9
Brandon Olynyk <brandontab2004@gmail.com>
2023-11-08 11:19:39 -0500

    Create seperate Event kt file for event menus

--- app/src/main/java/com/example/calendarapp/MainActivity.kt
+++ app/src/main/java/com/example/calendarapp/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.calendarapp.ui.screens.CreateEventMenu
 import com.example.calendarapp.ui.screens.DailyOverview
 import com.example.calendarapp.ui.theme.CalendarAppTheme
 
@@ -23,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    CreateEventMenu()
                 }
             }
         }
---
+++ app/src/main/java/com/example/calendarapp/ui/screens/Events.kt
@@ -0,0 +1,17 @@
+package com.example.calendarapp.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+    
+    @Composable
+    fun CreateEventMenu(){
+        Text(text = "test (if this works you have the CreateEvent menu shown yahee)")
+    }
+    @Preview
+    @Composable
+    fun CreateEventMenuPreview(){
+        CreateEventMenu()
+    }
+
