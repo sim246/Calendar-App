40f110a36c86077aff1a016bee73bea7a3a268f8
Simona Dragomir <simonadragomir2001@gmail.com>
2023-11-10 02:01:49 -0500

    working on layout

--- app/src/main/java/com/example/calendarapp/ui/screens/DailyOverviewPerspective.kt
+++ app/src/main/java/com/example/calendarapp/ui/screens/DailyOverviewPerspective.kt
@@ -1,48 +1,66 @@
 package com.example.calendarapp.ui.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.calendarapp.R
 
 @Composable
 fun DailyOverview() {
 
     var hour = 6;
-    var meridiem = "am"
+    Button(onClick = {}) {
+        Image(
+            painterResource(id = R.drawable.add_button),
+            contentDescription ="Cart button icon",
+            modifier = Modifier.size(20.dp))
+    }
     Column(
         horizontalAlignment = Alignment.Start,
         modifier = Modifier
             .background(MaterialTheme.colorScheme.background)
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
     )
     {
         var i = 0;
         while (i <= 18)
         {
-            Row(horizontalArrangement = Arrangement.Start)
+            Row(horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .height(50.dp)
+                    .border(BorderStroke(0.5.dp, Color.LightGray))
+                    .fillMaxWidth()
+            )
             {
-                if (hour != 12) {
-                    Text("$hour:00 $meridiem", color = MaterialTheme.colorScheme.primary)
-                } else {
-                    Text("$hour:00", color = MaterialTheme.colorScheme.primary)
-                }
+                Text("$hour:00", color = MaterialTheme.colorScheme.scrim)
             }
             i++
             if (hour == 12)
             {
                 hour = 1
-                meridiem = "pm"
             } else
             {
                 hour++
