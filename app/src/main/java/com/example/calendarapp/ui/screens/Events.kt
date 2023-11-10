d34334916d39b1d3ebeb6418b8d143d5882e9a53
Brandon Olynyk <brandontab2004@gmail.com>
2023-11-10 10:28:24 -0500

    Create event class and time picker state switching

--- app/src/main/java/com/example/calendarapp/ui/screens/Events.kt
+++ app/src/main/java/com/example/calendarapp/ui/screens/Events.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,7 +33,7 @@ import androidx.compose.ui.tooling.preview.Preview
             EventInputField("Title")
             EventInputField("Description")
             EventInputField("Location")
-            EventTimePicker()
+            EventTimeDisplay()
             Button(
                 content={Text(text = "Return to Menu")},
                 onClick={}
@@ -46,9 +47,28 @@ import androidx.compose.ui.tooling.preview.Preview
     }
 
     @Composable
-    fun EventTimePicker(){
-        Button(onClick={}, content={Text(text = "Time: ???? pm")},)
+    @ExperimentalMaterial3Api
+    fun EventTimeDisplay(){
+        var showTimePicker by remember { mutableStateOf(false) }
+        val time by rememberSaveable { mutableStateOf("12:24")}
+
+        Text("Time: $time")
+
+
+        if (showTimePicker) {
+
+            Button(onClick={showTimePicker = false}, content={Text(text = "Save")},)
+
+        }
+        else
+        {
+            Button(onClick={showTimePicker = true}, content={Text(text = "Set Time")},)
+        }
+
+
     }
+
+
 
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
