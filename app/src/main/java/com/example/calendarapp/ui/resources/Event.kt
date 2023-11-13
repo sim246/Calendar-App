242a428bbd88dd3762b9bfb4f5d7e2c8063dd3da
Brandon Olynyk <brandontab2004@gmail.com>
2023-11-13 11:01:09 -0500

    Add extra Event parameters

--- app/src/main/java/com/example/calendarapp/ui/resources/Event.kt
+++ app/src/main/java/com/example/calendarapp/ui/resources/Event.kt
@@ -5,5 +5,9 @@ import java.time.LocalDateTime
 class Event(
     val eventName: String,
     val start: LocalDateTime,
-    val end: LocalDateTime
+    val end: LocalDateTime,
+
+    val description: String,
+    val clientName: String,
+    val location: String
 )
