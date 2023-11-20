f003af63aafa22395ee5ae23bcbe7851332f3f04
Brandon Patrick Olynyk <brandon.olynyk@dawsoncollege.qc.ca>
2023-11-20 15:01:02 +0000

    Revert "Merge branch 'eventMenu' into 'main'"
    This reverts merge request !6

--- app/src/main/java/com/example/calendarapp/Routes.kt
+++
@@ -1,9 +0,0 @@
-package com.example.calendarapp
-
-sealed class Routes (val route: String) {
-    object MonthOverviewScreen : Routes("month")
-    object DailyOverview : Routes("day")
-    object EventOverview : Routes("event")
-    //View for a singular event, for editing / creating
-    object EventEdit : Routes("eventEdit")
-}
