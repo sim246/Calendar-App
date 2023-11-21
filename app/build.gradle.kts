4909b8b6c28042ee6c19f615e0aab4c1ee3bd7a6
Simona Dragomir <simonadragomir2001@gmail.com>
2023-11-21 00:02:02 -0500

    added back missing implements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
 
 dependencies {
 
+    implementation("com.android.tools:desugar_jdk_libs:1.0.9")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
@@ -59,13 +60,10 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-
     implementation("androidx.compose.foundation:foundation-android:1.5.4")
     implementation("com.google.android.libraries.places:places:3.2.0")
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
 
     implementation("androidx.test.ext:junit-ktx:1.1.5")
-
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -74,7 +72,6 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-
 
     implementation ("androidx.navigation:navigation-compose:2.5.3")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
