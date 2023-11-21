8ea8c57d5c8025b7a07addd9c11ea7ece2a96748
Simona Dragomir <simonadragomir2001@gmail.com>
2023-11-20 23:11:38 -0500

    tests

--- app/src/androidTest/java/com/example/calendarapp/DalyOverviewUnitTest.kt
+++ app/src/androidTest/java/com/example/calendarapp/DalyOverviewUnitTest.kt
@@ -1,25 +1,17 @@
 package com.example.calendarapp
 
 import android.os.Build
-import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
-import androidx.compose.ui.platform.LocalContext
-import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Rule
 import org.junit.Test
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.runner.RunWith
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.navigation.compose.rememberNavController
 import com.example.calendarapp.ui.presentation.screens.DailyOverview
-import com.example.calendarapp.ui.presentation.screens.ScreenSetup
 import com.example.calendarapp.ui.presentation.viewmodel.AppViewmodel
-import androidx.activity.viewModels
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
