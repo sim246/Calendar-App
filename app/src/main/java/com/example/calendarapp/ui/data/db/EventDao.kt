a7729dcc37e5913a5a3b05694af38c77538894b4
Simona Georgiana Dragomir <simona.dragomir@dawsoncollege.qc.ca>
2023-11-27 11:20:08 -0500

    insert and select events

--- app/src/main/java/com/example/calendarapp/ui/data/db/EventDao.kt
+++ app/src/main/java/com/example/calendarapp/ui/data/db/EventDao.kt
@@ -5,6 +5,8 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.calendarapp.ui.domain.Event
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 @Dao
 interface EventDao {
@@ -12,14 +14,17 @@ interface EventDao {
     fun insertEvent(event: Event)
 
     @Query("SELECT * FROM events WHERE eventName = :name")
-    fun findProduct(name: String): List<Event>
+    fun findEvents(name: String): List<Event>
 
     @Query("DELETE FROM events WHERE eventName = :name")
-    fun deleteProduct(name: String)
+    fun deleteEvents(name: String)
 
     @Query("SELECT * FROM events")
-    fun getAllProducts(): LiveData<List<Event>>
+    fun getAllEvents(): LiveData<List<Event>>
 
+    @Query("INSERT INTO events (day, eventName, start,theEnd, description, clientName, location) VALUES (:day, :eventName, :start, :end, :description, :clientName, :location)")
+    fun addEvent(day: LocalDate, eventName: String, start: LocalDateTime, end: LocalDateTime, description: String, clientName: String, location: String)
 
-
+    @Query("SELECT * FROM events WHERE day = :day")
+    fun getAllEventsByDay(day: LocalDate): LiveData<List<Event>>
 }
