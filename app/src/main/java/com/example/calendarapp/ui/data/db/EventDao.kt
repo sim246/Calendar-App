a5bf76747333fd0eefe33bfcc9d8fba3ddbb3325
Brandon Olynyk <brandontab2004@gmail.com>
2023-11-27 10:56:21 -0500

    Scaffhold DAO methods

--- app/src/main/java/com/example/calendarapp/ui/data/db/EventDao.kt
+++ app/src/main/java/com/example/calendarapp/ui/data/db/EventDao.kt
@@ -4,6 +4,20 @@ import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import com.example.calendarapp.ui.domain.Event
 
+@Dao
 interface EventDao {
+    @Insert
+    fun insertEvent(event: Event)
+    /*
+    @Query("SELECT * FROM events WHERE eventName = :name")
+    fun findProduct(name: String): List<Event>
+
+    @Query("DELETE FROM events WHERE eventName = :name")
+    fun deleteProduct(name: String)
+
+    @Query("SELECT * FROM events")
+    fun getAllProducts(): LiveData<List<Event>>
+    */
 }
