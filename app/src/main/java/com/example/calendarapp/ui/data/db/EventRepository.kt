dd58d5ec21c9ea7b58f8a31485ae870910a7cd1a
Brandon Olynyk <brandontab2004@gmail.com>
2023-11-27 12:31:20 -0500

    Further implement EventRepository

--- app/src/main/java/com/example/calendarapp/ui/data/db/EventRepository.kt
+++ app/src/main/java/com/example/calendarapp/ui/data/db/EventRepository.kt
@@ -3,9 +3,41 @@ package com.example.calendarapp.ui.data.db
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.calendarapp.ui.domain.Event
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class EventRepository (private val eventDao: EventDao) {
-    val allProducts: LiveData<List<Event>> = eventDao.getAllProducts()
-    val searchResults = MutableLiveData<List<Event>>()
 
+    val searchResults = MutableLiveData<List<Event>>()
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    fun insertProduct(newevent: Event) {
+        coroutineScope.launch(Dispatchers.IO) {
+            eventDao.insertEvent(newevent)
+        }
+    }
+
+    fun deleteProduct(name: String) {
+        coroutineScope.launch(Dispatchers.IO) {
+            eventDao.deleteEvents(name)
+        }
+    }
+
+    fun findProduct(name: String) {
+        coroutineScope.launch(Dispatchers.Main) {
+            searchResults.value = asyncFind(name).await()
+        }
+    }
+
+    private fun asyncFind(name: String): Deferred<List<Event>?> =
+        coroutineScope.async(Dispatchers.IO) {
+            return@async eventDao.findEvents(name)
+        }
 }
+
+
+
+
