c325d8d3a2ef565c95a4c2712a046510a2195068
Monica Dimitrova <monica.dimitrova@dawsoncollege.qc.ca>
2023-12-01 16:30:13 -0500

    Fix current day highlight

--- app/src/main/java/com/example/calendarapp/ui/presentation/screens/MonthOverview.kt
+++ app/src/main/java/com/example/calendarapp/ui/presentation/screens/MonthOverview.kt
@@ -134,12 +134,6 @@ fun DaysOfTheMonth(selectedMonth: YearMonth, navController: NavController, viewM
     val daysInMonth = selectedMonth.lengthOfMonth()
     val firstDayOfWeek = selectedMonth.atDay(1).dayOfWeek.value % 7 + 1
 
-    //val currentDay = Calendar.getInstance().firstDayOfWeek
-    //var startDay = ((firstDayOfWeek - currentDay + 8) % 7 + 1) % 7
-    //val startDay = (firstDayOfWeek - currentDay + 7) % 7 + 5
-
-    val currentDay = LocalDate.now().dayOfMonth
-    //Log.d("CURRENTDAY", currentDay.toString())
     val rows = ((daysInMonth + firstDayOfWeek - 1 + 6) / 7)
 
     for (row in 0 until rows) {
@@ -162,14 +156,16 @@ fun DaysOfTheMonth(selectedMonth: YearMonth, navController: NavController, viewM
 //                }
 
                  if (day in 1..daysInMonth) {
-                     //val hasEvent = daysWithEvents.contains(selectedMonth.atDay(day))
+                     val currentDate = LocalDate.now()
+                     val isCurrentDay = selectedMonth.atDay(day) == currentDate
+
                      val hasEvent = daysWithEvents?.map { it.toLocalDate() }?.contains(selectedMonth.atDay(day))
 
                      if (hasEvent == true) {
                         color = Color.DarkGray
                         fontColor = Color.White
                     }
-                     if(day == currentDay){
+                     else if(isCurrentDay){
                          color = Color.LightGray
                          fontColor = Color.White
                      }
